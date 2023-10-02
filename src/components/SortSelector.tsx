010d769fdb93a43d3db47d3f8eb44e23b0f41791
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-02 12:14:05 -0700

    build sort selector

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,33 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
+
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+
+const SortSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Order by: relevance
+      </MenuButton>
+      <MenuList>
+        <MenuItem>1</MenuItem>
+        <MenuItem>2</MenuItem>
+        <MenuItem>3</MenuItem>
+        <MenuItem>4</MenuItem>
+        <MenuItem>5</MenuItem>
+        <MenuItem>6</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
